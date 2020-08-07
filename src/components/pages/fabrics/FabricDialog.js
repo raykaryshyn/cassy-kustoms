04b4c5b591adbe554e7a5b6c8b50230a3e46ea86
Ray Karyshyn <20426749+RayKaryshyn@users.noreply.github.com>
2020-08-06 23:08:26 -0400

    Update

--- src/components/pages/fabrics/FabricDialog.js
+++ src/components/pages/fabrics/FabricDialog.js
@@ -20,6 +20,7 @@ export default function FabricDialog(props) {
   };
   const handleClose = () => {
     setOpen(false);
+    setLoad(false);
   };
 
   const descriptionElementRef = React.useRef(null);
