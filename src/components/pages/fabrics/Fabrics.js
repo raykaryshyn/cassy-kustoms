d50483abcb3db0d5aba340044419b9dd8465242f
Ray Karyshyn <20426749+RayKaryshyn@users.noreply.github.com>
2020-08-27 13:40:38 -0400

    Updates

--- src/components/pages/fabrics/Fabrics.js
+++ src/components/pages/fabrics/Fabrics.js
@@ -414,7 +414,7 @@ export default function Fabrics() {
             })
                 .then(() => {
                     console.log(body);
-                    /* cancelOrder(); */
+                    cancelOrder();
                 })
                 .catch(error => console.log(error));
         } else {
