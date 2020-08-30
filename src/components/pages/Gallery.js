1b02ba73174f699a2a89ba8d7562058a16c98240
Ray Karyshyn <20426749+RayKaryshyn@users.noreply.github.com>
2020-08-30 17:58:46 -0400

    Updates

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Modal from '@material-ui/core/Modal';
+import { IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Fade from '@material-ui/core/Fade';
+import Backdrop from '@material-ui/core/Backdrop';
 
 
 
@@ -41,18 +47,116 @@ export default function Gallery(props) {
             borderRadius: theme.shape.borderRadius,
             overflow: 'hidden',
         },
+
+
+        modal: {
+            overflowX: 'hidden',
+            overflowY: 'auto',
+            '& .MuiBackdrop-root': {
+                background: 'rgba(0,0,0,0.8)',
+            }
+        },
+        paperContainer: {
+            height: '100%',
+            outline: 0,
+            '&:after': {
+                width: 0,
+                height: '100%',
+                content: '""',
+                display: 'inline-block',
+                verticalAlign: 'middle',
+            },
+            padding: '66px 10px 0',
+            textAlign: 'center',
+            maxWidth: 1000,
+            position: 'relative',
+            margin: '0 auto',
+        },
+        paper: {
+            display: 'inline-block',
+            textAlign: 'center',
+            verticalAlign: 'middle',
+            '& img': {
+                display: 'block',
+                marginBottom: 10,
+                boxShadow: theme.shadows[5],
+                width: 'auto',
+                height: 'auto',
+                /* maxHeight: '100%', */
+                maxWidth: '100%',
+                maxHeight: 'calc(100vh - 90px)',
+            },
+        },
+        closeX: {
+            position: 'fixed',
+            top: 0,
+            left: '50%',
+            transform: 'translateX(calc(-50% - 16px))',
+            backgroundColor: theme.palette.background.paper,
+            color: '#222',
+            '&:hover': {
+                backgroundColor: theme.palette.background.paper,
+                color: '#222',
+            },
+            margin: 15,
+            padding: 8,
+            boxShadow: theme.shadows[5],
+        },
+        spinnerWrapper: {
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%,-50%)',
+            color: '#fff',
+            '& .MuiCircularProgress-root': {
+                color: '#fff',
+            },
+        },
     }));
     const classes = useStyles();
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const [didLoad, setLoad] = React.useState(false);
+    const style = didLoad ? {} : { opacity: 0 };
+    const style2 = didLoad ? { flex: 1 } : { visibility: 'hidden' };
 
     return (
         <>
             {props.urls.map((url, i) => {
                 return (
                     <Grid item xs={6} sm={4} md={3} key={i}>
-                        <div className={classes.galleryItemWrapper}><div style={{ backgroundImage: `url(${url.thumb})` }} className={classes.galleryItem}></div></div>
+                        <div className={classes.galleryItemWrapper}><div style={{ backgroundImage: `url(${url.thumb})` }} className={classes.galleryItem} onClick={handleOpen}></div></div>
                     </Grid>
                 );
             })}
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                className={classes.modal}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <div className={classes.paperContainer}>
+
+                        <IconButton aria-label="delete" className={classes.closeX} onClick={handleClose}>
+                            <CloseIcon />
+                        </IconButton>
+                    </div>
+                </Fade>
+            </Modal>
         </>
     );
 }
