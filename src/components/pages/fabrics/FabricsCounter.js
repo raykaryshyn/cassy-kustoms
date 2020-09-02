82f1a14830d7c021b6c791c82f63b82692143a57
Ray Karyshyn <20426749+RayKaryshyn@users.noreply.github.com>
2020-09-01 23:44:20 -0400

    Updates

--- src/components/pages/fabrics/FabricsCounter.js
+++ src/components/pages/fabrics/FabricsCounter.js
@@ -4,9 +4,11 @@ import RemoveIcon from '@material-ui/icons/Remove';
 import AddIcon from '@material-ui/icons/Add';
 import fabricsList from './fabricsList';
 import { FabricsContext } from './FabricsContext';
+import { makeStyles } from '@material-ui/core/styles';
 
 export default function FabricsCounter(props) {
     const context = React.useContext(FabricsContext).context;
+    const gridView = context.gridView;
     const orderFabrics = context.orderFabrics;
     const addOrderFabrics = context.addOrderFabrics;
     const removeOrderFabrics = context.removeOrderFabrics;
@@ -29,12 +31,48 @@ export default function FabricsCounter(props) {
     };
 
 
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            display: 'flex',
+            justifyContent: gridView ? 'center' : 'flex-start',
+            alignItems: 'center',
+            marginTop: gridView ? -2 : 6,
+            marginBottom: gridView ? 10 : 12,
+            marginLeft: gridView ? 0 : 6,
+            [theme.breakpoints.up(600)]: {
+                marginLeft: gridView ? 0 : 20,
+            },
+        },
+        button: {
+            background: theme.palette.primary.main,
+            color: '#fff',
+            padding: 3,
+            '&:hover': {
+                background: theme.palette.primary.main,
+                color: '#fff',
+            },
+            '&:first-of-type': {
+                marginRight: 8,
+            },
+            '&:last-of-type': {
+                marginLeft: 8,
+            },
+            '&.inactive': {
+                background: theme.palette.primary.light3,
+            }, 
+        },
+        amt: {
+            fontFamily: theme.typography.fonts.header,
+            fontSize: 20,
+        },
+    }));
+    const classes = useStyles();
 
     return (
-        <div>
-            <IconButton onClick={decreaseAmt}><RemoveIcon /></IconButton>
-            <span>{amt}</span>
-            <IconButton onClick={increaseAmt}><AddIcon /></IconButton>
+        <div className={classes.root}>
+            <IconButton onClick={decreaseAmt} className={[classes.button, amt === 0 ? 'inactive' : ''].join(' ')}><RemoveIcon /></IconButton>
+            <span className={classes.amt}>{amt}</span>
+            <IconButton onClick={increaseAmt} className={classes.button}><AddIcon /></IconButton>
         </div>
     );
 }
