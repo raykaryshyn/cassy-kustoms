a33cda58fdac894b82fc2c56d15d13d1f745a144
raykaryshyn <raykaryshyn@gmail.com>
2021-02-21 14:08:37 -0500

    Updates

--- src/components/pages/PhoneCases.js
+++ src/components/pages/PhoneCases.js
@@ -8,6 +8,10 @@ import Service from './Service';
 export default function PhoneCases() {
     const gallery = [
         {
+            thumb: '/gallery/phone-case6-thumbnail.jpg',
+            main: '/gallery/phone-case6.jpg',
+        },
+        {
             thumb: '/gallery/phone-case5-thumbnail.jpg',
             main: '/gallery/phone-case5.jpg',
         },
