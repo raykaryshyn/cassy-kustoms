450bdf7dfa24be1ea93c0406b40a3250a1138d7d
Ray Karyshyn <raykaryshyn@gmail.com>
2021-12-12 23:18:52 -0500

    updates

--- src/components/pages/Clothes.js
+++ src/components/pages/Clothes.js
@@ -6,7 +6,7 @@ import Service from './Service';
 
 
 export default function Clothes() {
-    const gallery = Array.from({ length: 26 }, (_, i) => i + 1).reverse().map(x => (
+    const gallery = Array.from({ length: 27 }, (_, i) => i + 1).reverse().map(x => (
         {
             thumb: `/gallery/clothes${x}-thumbnail.jpg`,
             main: `/gallery/clothes${x}.jpg`,
